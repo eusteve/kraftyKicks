71d2678a8d24ecb0c2575897acd160c51a8ed07a
eusteve <eusteve906@gmail.com>
2024-02-22 00:21:43 +0300

    Updated images folder , addded the slide

--- main.js
+++ main.js
@@ -26,3 +26,31 @@ document.getElementById("subscribeForm").addEventListener("submit", function(eve
     // Add your subscription logic here
     alert("Thank you for subscribing!");
   });
+
+  /*slideShow */
+  // substitutes the carousel
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+  }
